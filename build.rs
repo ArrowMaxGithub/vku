d550bc7115ccb57077df2af0b7591a61a674d701
Max Pfeil <mail-maxpfeil@web.de>
2023-04-09 22:01:15 +0200

    build script fix

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use copy_to_output::copy_to_output;
 use std::env;
 
 fn main() {
-    println!("cargo:rerun-if-changed=res/*");
+    println!("cargo:rerun-if-changed=assets/*");
     copy_to_output("assets", &env::var("PROFILE").unwrap()).expect("Could not copy");
 }
