c51f4940394b2e6a9d55c402a9875e2638cad007
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2025-07-27 11:46:43 +0200

    add unsupported layout transition error variant

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use ash::vk::ImageLayout;
 use std::{ffi::NulError, str::Utf8Error};
 use thiserror::Error;
 
@@ -12,7 +13,7 @@ pub enum Error {
     HeadCallOnHeadlessInstance,
     #[error("no suitable GPU was found to create the physical device")]
     NoSuitableGPUFound,
-    #[error("device extension was requested but is not supported. Extension: {0}")]
+    #[error("device extension was requested but is not supported: {0}")]
     RequiredDeviceExtensionNotSupported(String),
     #[error("requested surface format is not supported by the surface")]
     RequestedSurfaceFormatNotSupported,
@@ -21,8 +22,8 @@ pub enum Error {
     #[error("requested present mode is not supported by the surface")]
     PresentModeNotSupported,
 
-    #[error("the requested image layout transition is not supported")]
-    UnsupportedImageLayoutTransition,
+    #[error("the requested image layout transition is not supported from: {0:?} to: {1:?}")]
+    UnsupportedImageLayoutTransition(ImageLayout, ImageLayout),
     #[error("tried to set data on an unmapped buffer")]
     WriteAttemptToUnmappedBuffer,
 
@@ -30,33 +31,33 @@ pub enum Error {
     UnknownShaderFileExtension,
 
     #[cfg(feature = "shader")]
-    #[error("shader compilation failed, see preprocess trace above. Source error: {0}")]
+    #[error("shader compilation failed, see preprocess trace above")]
     Preprocess(#[from] shaderc::Error),
 
-    #[error("incorrect usage of the vulkan API: {0}")]
+    #[error("incorrect usage of the vulkan API")]
     Vk(#[from] ash::vk::Result),
 
-    #[error("vulkan entry could not be loaded: {0}")]
+    #[error("vulkan entry could not be loaded")]
     AshLoad(#[from] ash::LoadingError),
 
-    #[error("utf8 error: {0}")]
+    #[error("utf8 error")]
     Utf8(#[from] Utf8Error),
 
-    #[error("cstring convert error: {0}")]
+    #[error("cstring convert error")]
     CStringConvert(#[from] NulError),
 
-    #[error("io error: {0}")]
+    #[error("io error")]
     IO(#[from] std::io::Error),
 
-    #[error("gpu allocation error: {0}")]
+    #[error("gpu allocation error")]
     GpuAlloc(#[from] gpu_allocator::AllocationError),
 
     #[error("shaderc failed to initialize")]
     ShaderCInit,
 
-    #[error("raw window handle error: {0}")]
+    #[error("raw window handle error")]
     RawWindowHandle(#[from] raw_window_handle::HandleError),
 
-    #[error("encountered an unknown error: {0}")]
+    #[error("encountered an unknown error")]
     Catch(#[from] Box<dyn std::error::Error>),
 }
